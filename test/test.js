2e44de081b3f2aa53c4d69462652b51c18c6e662
rchennuri <rchennuri83@gmail.com>
2016-01-03 12:05:31 +0530

    Add node app

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Raj on 03-01-2016.
+ */
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function() {
+    it('respond with hello world', function(done) {
+        request(app).get('/').expect('hello world', done);
+    });
+});
